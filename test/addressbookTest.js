e43f114b45279b37aa682811de873cce67866a30
Milen Radkov <milenradkov@me.com>
2019-12-24 18:56:07 +0200

    update and fix test cases

--- test/addressbookTest.js
+++ test/addressbookTest.js
@@ -14,107 +14,65 @@
  *  OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
  *  PERFORMANCE OF THIS SOFTWARE.
  */
-const Ae = require('@aeternity/aepp-sdk').Universal;
-const Crypto = require('@aeternity/aepp-sdk').Crypto;
 
-const config = {
-  host: "http://localhost:3001/",
-  internalHost: "http://localhost:3001/internal/",
-  contractSourcePath: "./contracts/AddressBook.aes",
-  gas: 200000,
-  ttl: 55
-}
+const Universal = require('@aeternity/aepp-sdk').Universal;
+const MemoryAccount = require('@aeternity/aepp-sdk').MemoryAccount;
+const CONTRACT_SOURCE = utils.readFileRelative('./contracts/address-book.aes', 'utf-8');
 
-describe('AddressBook Contract', () => {
+describe('Address Book Contract', () => {
 
-  let owner;
-  let nonOwner;
-  let contractSource;
-  let compiledContract;
-  let contractInstance;
+    let contract, client;
 
-  before(async () => {
-    owner = await Ae({
-      url: config.host,
-      internalUrl: config.internalHost,
-      keypair: wallets[0],
-      nativeMode: true,
-      networkId: 'ae_devnet'
+    before(async () => {
+        client = await Universal({
+            url: "http://localhost:3001",
+            internalUrl: "http://localhost:3001/internal",
+            accounts: [
+                MemoryAccount({keypair: wallets[0]}),
+                MemoryAccount({keypair: wallets[1]}),
+                MemoryAccount({keypair: wallets[2]}),
+                MemoryAccount({keypair: wallets[3]})
+            ],
+            networkId: "ae_devnet",
+            compilerUrl: "http://localhost:3080"
+        })
     });
 
-    nonOwner = await Ae({
-      url: config.host,
-      internalUrl: config.internalHost,
-      keypair: wallets[1],
-      nativeMode: true,
-      networkId: 'ae_devnet'
+    beforeEach(async () => {
+        contract = await client.getContractInstance(CONTRACT_SOURCE);
+        const init = await contract.deploy([]);
+        assert.equal(init.result.returnType, 'ok');
     });
 
-    // Read the source file
-    contractSource = utils.readFileRelative(config.contractSourcePath, "utf-8");
-    compiledContract = await owner.contractCompile(contractSource, { // Compile it
-      gas: config.gas
-    })
-
-    contractInstance = await compiledContract.deploy({ // Deploy it
-      options: {
-        ttl: config.ttl,
-      },
-      abi: "sophia"
+    it('Deploy AddressBook Contract', async () => {
+        contract = await client.getContractInstance(CONTRACT_SOURCE);
+        const deploy = await contract.deploy([]);
+        assert.equal(deploy.result.returnType, 'ok');
     });
-  })
 
-  it('Deploying AddressBook Contract', async () => {
-    assert(contractInstance, 'Could not deploy the AddressBook Smart Contract'); // Check it is deployed
-  })
-
-  it('Should store provided person correctly', async () => {
-    let address = "0x" + Crypto.decodeBase58Check(wallets[0].publicKey.split('_')[1]).toString('hex');  //wallets[0]; 
-    let first_name = "Milen";
-    let last_name = "Radkov";
-    let age = 25;
-
-    // Call add Person function
-    const addPerson = await owner.contractCall(compiledContract.bytecode, 'sophia', contractInstance.address, "addPerson", {
-      args: `(${address}, "${first_name}", "${last_name}", ${age})`,
-      options: {
-        ttl: config.ttl
-      },
-      abi: "sophia"
+    it('Should store provided person correctly', async () => {
+      let dummyPerson = {
+        firstName: 'Dummy',
+        lastName: 'Person',
+        age: 42
+      }
+      let result = await contract.methods.add_person(wallets[1].publicKey, dummyPerson.firstName, dummyPerson.lastName, dummyPerson.age);
+      assert.equal(result.result.returnType, 'ok');
     })
 
-    const addPersonResult = await addPerson.decode('bool');
-    assert.equal(addPersonResult.value, true, "Person is not added successfully");
-  })
+    it('Should read person correctly', async () => {
+      let dummyPerson = {
+        firstName: 'Dummy',
+        lastName: 'Person',
+        age: 42
+      }
+      let result = await contract.methods.add_person(wallets[1].publicKey, dummyPerson.firstName, dummyPerson.lastName, dummyPerson.age);
+      assert.equal(result.result.returnType, 'ok');
 
-  it('Should get person data correctly correctly', async () => {
-    let address = "0x" + Crypto.decodeBase58Check(wallets[0].publicKey.split('_')[1]).toString('hex'); 
-    let first_name = "Milen";
-    let last_name = "Radkov";
-    let age = 25;
+      let read = await contract.methods.get_person(wallets[1].publicKey);
 
-    // Call addPerson function
-    const addPerson = await owner.contractCall(compiledContract.bytecode, 'sophia', contractInstance.address, "addPerson", {
-      args: `(${address}, "${first_name}", "${last_name}", ${age})`,
-      options: {
-        ttl: config.ttl
-      },
-      abi: "sophia"
+      assert.equal(dummyPerson.firstName, read.decodedResult.first_name, 'First name does not match');
+      assert.equal(dummyPerson.lastName, read.decodedResult.last_name, 'Last name does not match');
+      assert.equal(dummyPerson.age, read.decodedResult.age, 'Age does not match');
     })
-
-    // Call getPerson function
-    const getPerson = await owner.contractCall(compiledContract.bytecode, 'sophia', contractInstance.address, "getPerson", {
-      args: `(${address})`,
-      options: {
-        ttl: config.ttl
-      },
-      abi: "sophia"
-    })
-
-   	const getPersonResult = await getPerson.decode('(string,string,int)');
-	
-	assert.equal(getPersonResult.value[0].value, first_name, "Person first name does not match");
-	assert.equal(getPersonResult.value[1].value, last_name, "Person last name not match");
-	assert.equal(getPersonResult.value[2].value, age, "Person age does not match");
-  })
-})
+});
